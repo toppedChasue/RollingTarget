454a21d551e765fe413e972ea919a0ba4d3ee1c4
toppedChasue <104607816+toppedChasue@users.noreply.github.com>
2022-11-07 14:37:56 +0900

    1234

---
+++ Assets/Scripts/Exp.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exp: MonoBehaviour
+{
+    //나타나면 플레이어쪽으로 날아오기
+
+
+    public Transform startPos;
+
+
+    private void Start()
+    { 
+        startPos = this.transform;
+    }
+
+    private void Update()
+    {
+        
+
+    }
+
+
+}
